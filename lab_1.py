f30839ec496ba4d85fb9cba73d6290ef88f73083
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-09-18 10:28:25 +0700

    working on exponent

--- lab_1.py
+++ lab_1.py
@@ -17,6 +17,29 @@ def double_epsilon():
     return eps_last
 
 def exponent_range(dtype):
+   e_max = 0
+   value = dtype(1.0)
+   while True:
+       test_value = value * dtype(2.0)
+       if (np.isinf(test_value)):
+           break
+       value = test_value
+       e_max += 1
+    
+   e_min = 0
+   value = dtype(1.0)
+   while True:
+       test_value = value / dtype(2.0)
+       if (test_value == dtype(0.0)):
+           break 
+       value = test_value
+       e_min -= 1
+       
+   return e_min + 1 , e_max - 1
+
+def actual_exponent_range(dtype):
+    info = np.finfo(dtype)
+    return info.minexp , info.maxexp
 
 
 epsilon_32 = single_epsilon()
@@ -27,4 +50,15 @@ t_64 = -math.log2(epsilon_64)
 
 print(t_32)
 print(t_64)
+
+e_min_32 , e_max_32 = exponent_range(np.float32)
+e_min_64 , e_max_64 = exponent_range(np.float64)
+
+actual_e_min_32 , actual_e_max_32 = (actual_exponent_range(np.float32))
+actual_e_min_64 , actual_e_max_64 = (actual_exponent_range(np.float64))
+
+print(f"experemetnal e_min_32 = {e_min_32} || actual e_min_32 = {actual_e_min_32}")
+print(f"experemental e_max_32 = {e_max_32} || actual e_max_32 = {actual_e_max_32}")
+print(f"experemental e_min_64 = {e_min_64} || actual e_min_64 = {actual_e_min_64}")
+print(f"experemental e_max_64 = {e_max_64} || actual e_max_64 = {actual_e_max_64}")
 
